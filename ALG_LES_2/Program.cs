41d1320013d709dc0f271698a1d99ad0e9f6bb81
Pavmy <pavmy@hotmail.com>
2018-09-30 20:49:11 +0300

    Перевод из 10 системы на Си

--- ALG_LES_2/Program.cs
+++ ALG_LES_2/Program.cs
@@ -4,6 +4,43 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+//Павлюков Михаил
+//#include "stdio.h"
+//#include "conio.h"
+//#include "locale.h"
+
+//int main(int argc, char* argv[])
+//{
+//    setlocale(0, "rus");
+//    int a, i, mass[99];
+//    for (i = 0; i < 99; i++)
+//    {
+//        mass[i] = 0;
+//    }
+//    printf("Введите число: ");
+//    scanf_s("%d", &a);
+//    i = 0;
+//    while (a != 0)
+//    {
+//        if (a % 2 == 0)
+//        {
+//            mass[i] = 0;
+//            a = a / 2;
+//        }
+//        else
+//        {
+//            mass[i] = 1;
+//            a = a / 2;
+//        }
+//        i++;
+//    }
+//    i = i - 1;
+//    for (; i >= 0; i--)
+//        printf("%d", mass[i]);
+//    _getch();
+//    return 0;
+//}
+
 namespace ALG_LES_2
 {
     class Program
